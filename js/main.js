ff8600ce4016f285a9c4aede9695f8969a81cd50
Lele <nicolas.lcorbalan@davinci.edu.ar>
2024-09-25 00:10:31 -0300

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function btnComprar(){
+    alert("Por el momento este botón no ejecuta ninguna accion, así que no será posible comprar su Nintendo Switch - Edicion Oled :(");
+}
+
+$(".galeria").cycle({
+    fx: 'fade',
+    speed: 1000,
+    timeout: 2000,
+    pause: false
+});
